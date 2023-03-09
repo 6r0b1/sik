da9513908d6f6114a7bbc7aaffd24048a6eca9c3
Veit Hueter <info@pinkblau.de>
2023-03-09 15:25:32 +0100

    build prep

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx";
 
 export default function Home() {
     return (
