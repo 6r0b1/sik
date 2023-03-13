33a955a1c413b603b5b89a35803891c4b395d297
Veit Hueter <info@pinkblau.de>
2023-03-13 12:10:06 +0100

    works details

--- types/interfaces.ts
+++ types/interfaces.ts
@@ -2,4 +2,10 @@ export interface IWork {
     id: number;
     title: string;
     description: string;
+    description_long?: string;
+    dates?: [{ date: string; venue: string }];
+    main_img_src?: string;
+    img_src?: [string];
+    credits_role?: [string];
+    credits_name?: [string];
 }
