4272db4c5dc34b1d1fa796c63df745633d8b94ad
Veit Hueter <info@pinkblau.de>
2023-05-09 22:02:45 +0200

    login off

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -3,21 +3,21 @@ import { withSessionRoute } from "../../lib/config/withSession";
 export default withSessionRoute(createSessionRoute);
 
 async function createSessionRoute(req, res) {
-    if (req.method === "POST") {
-        const { userName, password } = req.body;
+    // if (req.method === "POST") {
+    // const { userName, password } = req.body;
 
-        if (
-            userName === process.env.ADMIN_NAME &&
-            password === process.env.ADMIN_PASSWORD
-        ) {
-            req.session.user = {
-                username: "admin",
-                isAdmin: true,
-            };
-            await req.session.save();
-            res.send({ ok: true });
-        }
-        return res.status(403).send("");
-    }
-    return res.status(401).send("nope");
+    // if (
+    //     userName === process.env.ADMIN_NAME &&
+    //     password === process.env.ADMIN_PASSWORD
+    // ) {
+    //     req.session.user = {
+    //         username: "admin",
+    //         isAdmin: true,
+    //     };
+    //     await req.session.save();
+    //     res.send({ ok: true });
+    // }
+    return res.status(403).send("nope");
+    // }
+    // return res.status(401).send("nope");
 }
