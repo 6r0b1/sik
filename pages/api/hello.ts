42c502fe451fe0d99160f59e68981c43515a478b
Veit Hueter <info@pinkblau.de>
2023-05-26 10:20:50 +0200

    db test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -12,32 +12,32 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    // const data = {
-    //     title_de: "test",
-    //     description_de: "test",
-    //     title_en: "test",
-    //     description_en: "test",
-    //     main_img_src: "test.jpg",
-    // };
-    // const dbTest = await prismaClient.works.create({
-    //     data: data,
-    // });
-    // console.log(dbTest);
-    // // const dbTest: Works = await prismaClient.works.findMany();
-    // type worksWithImages = Prisma.WorksGetPayload<{
-    //     include: {
-    //         work_images: true;
-    //     };
-    // }>;
-    // const testQuery: worksWithImages = await prismaClient.Works.findUnique({
-    //     where: {
-    //         id: 1,
-    //     },
-    //     include: {
-    //         work_images: true,
-    //     },
-    // });
-    // console.log(testQuery);
+    const data = {
+        title_de: "test",
+        description_de: "test",
+        title_en: "test",
+        description_en: "test",
+        main_img_src: "test.jpg",
+    };
+    const dbTest = await prismaClient.works.create({
+        data: data,
+    });
+    console.log(dbTest);
+    // const dbTest: Works = await prismaClient.works.findMany();
+    type worksWithImages = Prisma.WorksGetPayload<{
+        include: {
+            work_images: true;
+        };
+    }>;
+    const testQuery: worksWithImages = await prismaClient.Works.findUnique({
+        where: {
+            id: 1,
+        },
+        include: {
+            work_images: true,
+        },
+    });
+    console.log(testQuery);
 
     res.status(200).json({ name: "John Doe" });
 }
